63c3cb0a695cc45cdd1a625c03e1092308e8d21e
Angelie <angelie.angman@gmail.com>
2026-02-04 14:51:13 +0100

    uppdatera ts

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,50 @@
 // @ts-nocheck
 import "./style.css";
 
-// Definiera variabler som pekar på "Lägg till" knapparna
-const addIncomeItem = document.querySelector("#addIncomeItem");
-const addExpenseItem = document.querySelector("#addExpenseItem");
-// Definiera variabler som pekar på input-fälten
+// Definiera globala variabler som pekar på "Lägg till" knapparna
+const addIncomeItemBtn = document.querySelector("#addIncomeItemBtn");
+const addExpenseItemBtn = document.querySelector("#addExpenseItemBtn");
+
+// Definiera globala variabler som pekar på input-fälten
+const incomeDescriptionInput = document.querySelector("#incomeDescription");
+const incomeAmountInput = document.querySelector("#incomeAmount");
+const expenseDescriptionInput = document.querySelector("#expenseDescription");
+const expenseAmountInput = document.querySelector("#expenseAmount");
 
 // Lyssna efter klick på "Lägg till" knappen
-addIncomeItem?.addEventListener("click", readIncomeInput);
-addExpenseItem?.addEventListener("click", readExpenseInput);
+addIncomeItemBtn.addEventListener("click", createIncomeBudgetPostOnClick);
+addExpenseItemBtn.addEventListener("click", createExpenseBudgetPostOnClick);
 
-// Funktion som läser värden från input-fälten
-function readIncomeInput() {
+// Läs in, spara och skapa budgetpost av inmatat värde
+function createIncomeBudgetPostOnClick() {
   console.log("Inkomst-knappen funkar!");
+  // 1. läs värden från input-fälten och spara dom i lokala variabler
+  const description = incomeDescriptionInput?.value;
+  const amount = incomeAmountInput?.value;
+  // Testa så det funkar!
+  console.log(description);
+  console.log(amount);
+  // 2. Skapar budgetpost
+  const budgetPost = {
+    description: description,
+    amount: amount,
+  };
+  console.log(budgetPost);
+  // 3. Rendera
 }
 
-function readExpenseInput() {
+function createExpenseBudgetPostOnClick() {
   console.log("Utgift-knappen funkar!");
+
+  const description = expenseDescriptionInput?.value;
+  const amount = expenseAmountInput?.value;
+
+  console.log(description);
+  console.log(amount);
+
+  const budgetPost = {
+    description: description,
+    amount: amount,
+  };
+  console.log(budgetPost);
 }
